645df1c5edd736f144031415d00bddbaddd1cf85
TaufikAkbar7 <taufik.lol911@gmail.com>
2021-07-07 16:22:30 +0700

    install tailwind

---
+++ client/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+
+    const api_url = "https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+    return (
+        <div>
+            <a href={api_url} className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                Login Spotify
+            </a>
+        </div>
+    )
+}
+
+export default Login
